403af89f87bf10626b52cd4e6b89b36a9a690df7
Suresha Ramappa <suresh_ramappa@condenast.com>
2021-10-10 09:40:26 +0530

    Initial code

---
+++ server.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+
+  server.route([
+    {
+      method: "GET",
+      path: "/",
+      handler: (request, h) => {
+        return "Hello from Hapi";
+      },
+    },
+    {
+      method: "GET",
+      path: "/user",
+      handler: (request, h) => {
+        return { name: "Suresh", ciry: "Bangalore" };
+      },
+    },
+  ]);
+
+  await server.start();
+  console.log(`Server is running at ${server.info.uri}`);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
